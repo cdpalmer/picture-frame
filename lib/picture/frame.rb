4be59a7174a7c6a3f8e744d59664332019453085
Cody Palmer <cody.palmer08@gmail.com>
2016-02-26 10:00:12 -0700

    Initial commit: Hackday, baby ;

---
+++ lib/picture/frame.rb
@@ -0,0 +1,7 @@
+require "picture/frame/version"
+
+module Picture
+  module Frame
+    # Your code goes here...
+  end
+end
---
+++ lib/picture/frame/version.rb
@@ -0,0 +1,5 @@
+module Picture
+  module Frame
+    VERSION = "0.1.0"
+  end
+end
